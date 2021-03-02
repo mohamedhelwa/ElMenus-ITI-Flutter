774a3814f27c91bbc8900da27263abfe4f6c13de
Mohamed <moh_helwa@hotmail.com>
2021-03-02 17:10:35 +0200

    Adding App Icons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:elmenus_iti/main.dart';
+import 'package:ElMenus_ITI/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
